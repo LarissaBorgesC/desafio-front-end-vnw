545d42dbfcbcb04cfbabe6acf69d0c5b9935099c
Larissa <lariborgesc7@gmail.com>
2024-12-03 14:20:11 -0300

    footer

--- src/componente/Footer/footer.jsx
+++ src/componente/Footer/footer.jsx
@@ -1,4 +1,4 @@
-import Facebook from '../../assets/F.png'
+import Facebook from '../../assets/Facebook.png'
 import Twitter from '../../assets/X.png'
 import Youtube from '../../assets/y.png'
 import Linkedin from '../../assets/Lin.png'
