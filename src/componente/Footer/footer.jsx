3ed64067f36680082bb8da9514c7533e6bf119ae
Larissa <lariborgesc7@gmail.com>
2024-12-03 13:05:35 -0300

    footer

--- src/componente/Footer/footer.jsx
+++ src/componente/Footer/footer.jsx
@@ -13,11 +13,11 @@ export default function footer(){
             <section className={S.boxmidias}>
                 <p>4002-8922</p>
                 <nav>
-                   <a href='https://www.facebook.com/'><img src={Facebook} alt="Facebook" width={40}/> </a>
-                   <a href='https://x.com/?logout=1732720737380'><img src={Twitter} alt="Twitter" width={40} /> </a>
-                   <a href='https://www.youtube.com/'><img src={Youtube} alt="Youtube" width={40} /> </a>
-                   <a href='https://br.linkedin.com/'><img src={Linkedin} alt="Linkedin" width={40} /> </a>
-                   <a href='https://www.instagram.com/'><img src={Instagram} alt="Instagram" width={40} /> </a>
+                   <a href='https://www.facebook.com/'><img src={Facebook} alt="Facebook" /> </a>
+                   <a href='https://x.com/?logout=1732720737380'><img src={Twitter} alt="Twitter"  /> </a>
+                   <a href='https://www.youtube.com/'><img src={Youtube} alt="Youtube"  /> </a>
+                   <a href='https://br.linkedin.com/'><img src={Linkedin} alt="Linkedin"  /> </a>
+                   <a href='https://www.instagram.com/'><img src={Instagram} alt="Instagram" /> </a>
                 </nav>
                 
             </section>
