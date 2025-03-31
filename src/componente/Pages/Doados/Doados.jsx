39c3444fd72d6150276eedeebaed489e2e1c93b2
Larissa <lariborgesc7@gmail.com>
2025-03-31 23:44:11 +0200

    get e post

--- src/componente/Pages/Doados/Doados.jsx
+++ src/componente/Pages/Doados/Doados.jsx
@@ -1,7 +1,29 @@
 import Livrologica from '../../../assets/Logicadapro.webp'
 import Livrotecnica from '../../../assets/Tecnicasdepro.webp'
 import S from '../Doados/Doados.module.scss'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+
+
+
 export default function Doados(){
+    
+    const [livros, setLivros] = useState([])
+
+    const getLivros = async () =>{
+        const response = await axios.get("https://apilivros-qtdc.onrender.com/livros")
+
+        setLivros(response.data)
+    }
+
+    useEffect( ()=> {
+        getLivros()
+    },[])
+
+
+
+
     return(
         <>
         <section className={S.boxsection} >
@@ -14,6 +36,8 @@ export default function Doados(){
                 <p>Tecnologia</p>
                 <p>João Rubens Marchete Filho</p>
             </article>
+
+
             <article className={S.boxlivro2}>
            
                 <img src={Livrotecnica} alt="Livro"   />
@@ -21,6 +45,16 @@ export default function Doados(){
                 <p>Tecnologia da Informação</p>
                 <p>Cassiana Fagundes da Silva</p>
             </article>
+
+            {livros.map((item)=>(
+                <article> 
+                    <img src={item.imagem_url} alt='Capa do Livro' />
+                    <h3>{item.titulo}</h3>
+                    <p>{item.categoria}</p>
+                    <p>{item.autor}</p>
+                </article>
+
+            ))}
         </section>
         </>
     )
--- src/componente/Pages/QueroDoar/QueroDoar.jsx
+++ src/componente/Pages/QueroDoar/QueroDoar.jsx
@@ -1,8 +1,65 @@
 import S from '../QueroDoar/QueroDoar.module.scss'
 import infolivro from '../../../assets/infolivro.jpg'
+import axios from 'axios'
+import { useState } from 'react'
 
 
 export default function QueroDoar(){
+
+    const [titulo, setTitulo] = useState("")
+    const [categoria, setCategoria] = useState("")
+    const [autor, setAutor] = useState("")
+    const [imagem_url, setImagem_url] = useState("")
+
+
+    const enviarDados = async()=>{
+
+        const urlApi = "https://apilivros-qtdc.onrender.com/doar"
+
+        const dadosEnviar = {
+            titulo,
+            categoria,
+            autor,
+            imagem_url
+        }
+
+        await axios.post(urlApi, dadosEnviar)
+        alert("Livro cadastrado!")
+
+        setTitulo("")
+        setCategoria("")
+        setAutor("")
+        setImagem_url("")
+
+
+
+    }
+
+
+
+
+    const capturaTitulo  = (e) => {
+        setTitulo(e.target.value)
+ 
+    }
+    const capturaCategoria  = (e) => {
+        setCategoria(e.target.value)
+ 
+    }
+    const capturaAutor  = (e) => {
+        setAutor(e.target.value)
+ 
+    }
+    const capturaImagem  = (e) => {
+        setImagem_url(e.target.value)
+ 
+    }
+
+
+
+
+
+
     return(
         <>
             <section className={S.boxconfprin}>
@@ -13,7 +70,7 @@ export default function QueroDoar(){
 
                 </p>
             
-            <form action='' className={S.boxform}>
+            <form action='' className={S.boxform} onSubmit={(e)=> e.preventDefault()}>
             
             <div >
                 <img src={infolivro} alt='Livro' width={50} height={40} />
@@ -23,11 +80,11 @@ export default function QueroDoar(){
          
 
 
-                <input type="text" id="Título" placeholder="Título" />
-                <input type="text" id="Categoria" placeholder="Categoria" />
-                <input type="text" id="Autor" placeholder="Autor" />
-                <input type="link" id="linkdaimagem" placeholder="Link da imagem" />
-                <input type='submit' value="Doar"/>
+                <input type="text" id="Título" placeholder="Título" onChange={capturaTitulo} value={titulo}/>
+                <input type="text" id="Categoria" placeholder="Categoria" onChange={capturaCategoria} value={categoria}/>
+                <input type="text" id="Autor" placeholder="Autor" onChange={capturaAutor} value={autor}/>
+                <input type="link" id="linkdaimagem" placeholder="Link da imagem" onChange={capturaImagem} value={imagem_url}/>
+                <input type='submit' value="Doar" onClick={enviarDados}/>
             
 
             </form>
